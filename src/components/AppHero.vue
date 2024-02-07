7d693681715a25ea976a2dee4a8d647844c4c175
LorenzoRossini01 <lory.rox.01@gmail.com>
2024-02-07 16:22:22 +0100

    fix: ordine cartelle

---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      imgName: "img01.png",
+    };
+  },
+
+  methods: {
+    getImgUrl(img) {
+      return new URL(`../assets/${img}`, import.meta.url).href;
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <img src="../assets/img01.png" alt="" />
+  </div>
+</template>
+
+<style></style>
